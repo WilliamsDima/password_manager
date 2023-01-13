4c0d011ad41ceb1c71cf336ef850e6f806bbeea2
@dlemetin <dlemetin@kameleoon.com>
2023-01-13 16:58:44 +0300

    add animation for navigation

--- src/navigation/childrens/auth_routes.tsx
+++ src/navigation/childrens/auth_routes.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { createStackNavigator } from "@react-navigation/stack"
 import { RoutesNames } from "../routes-names"
-import { screenOptions } from "../routes-config"
+import { screenOptions, stackOptions } from "../routes-config"
 import Auth from "../../screens/Auth"
 import Pin from "../../screens/Pin"
 import Start from "../../screens/Start"
@@ -52,14 +52,36 @@ const AuthRoutes = () => {
 		>
 			{!start && !pin && (
 				<>
-					<AuthStack.Screen name={RoutesNames.Start} component={Start} />
-					<AuthStack.Screen name={RoutesNames.Pin} component={Pin} />
+					<AuthStack.Screen
+						name={RoutesNames.Start}
+						options={stackOptions}
+						component={Start}
+					/>
+					<AuthStack.Screen
+						name={RoutesNames.Pin}
+						options={stackOptions}
+						component={Pin}
+					/>
 				</>
 			)}
 
-			{pin && <AuthStack.Screen name={RoutesNames.Pin} component={Pin} />}
-			<AuthStack.Screen name={RoutesNames.Auth.AuthStack} component={Auth} />
-			<AuthStack.Screen name={RoutesNames.Auth.Register} component={Register} />
+			{pin && (
+				<AuthStack.Screen
+					options={stackOptions}
+					name={RoutesNames.Pin}
+					component={Pin}
+				/>
+			)}
+			<AuthStack.Screen
+				options={stackOptions}
+				name={RoutesNames.Auth.AuthStack}
+				component={Auth}
+			/>
+			<AuthStack.Screen
+				options={stackOptions}
+				name={RoutesNames.Auth.Register}
+				component={Register}
+			/>
 		</AuthStack.Navigator>
 	)
 }
--- src/navigation/routes-config.ts
+++ src/navigation/routes-config.ts
@@ -1,9 +1,33 @@
 export const screenOptions = {
-  headerStyle: {
-    borderBottomWidth: 0,
-    // android
-    elevation: 0,
-    // ios
-    shadowOpacity: 0,
-  },
+	headerStyle: {
+		borderBottomWidth: 0,
+		// android
+		elevation: 0,
+		// ios
+		shadowOpacity: 0,
+	},
+}
+
+const horizontalAnimation = {
+	cardStyleInterpolator: ({ current, layouts }) => {
+		return {
+			cardStyle: {
+				transform: [
+					{
+						translateX: current.progress.interpolate({
+							inputRange: [0, 1],
+							outputRange: [layouts.screen.width, 0],
+						}),
+					},
+				],
+			},
+		}
+	},
+}
+
+export const stackOptions = {
+	...horizontalAnimation,
+	presentation: "transparentModal",
+	animationTypeForReplace: "push",
+	animation: "slide_from_left",
 }
